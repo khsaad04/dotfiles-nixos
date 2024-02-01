a3c5a1fc485c5b292576b236b1475f5decc9de64
khsaad04 <khsaad0404@gmail.com>
2024-02-01 19:14:22 +0600

    minor refactor

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,21 +8,6 @@
     ./neovim
   ];
 
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     vim
