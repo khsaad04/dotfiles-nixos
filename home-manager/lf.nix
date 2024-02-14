2686f3371c07c79d5938c8cdd0681ef6f66aee0c
khsaad04 <khsaad0404@gmail.com>
2024-02-14 18:10:07 +0600

    add text opening support to lf

--- home-manager/lf.nix
+++ home-manager/lf.nix
@@ -4,19 +4,7 @@
   ...
 }: {
   xdg.configFile."lf/icons".source = "${inputs.lf-icons}/etc/icons.example";
-  programs.lf = let
-    previewer = pkgs.writeShellScriptBin "previewer" ''
-      case "$(${pkgs.file}/bin/file -Lb --mime-type -- "$1")" in
-          image/*)
-              ${pkgs.chafa}/bin/chafa -f sixel -s "$2x$3" --animate off --polite on "$1"
-              exit 1
-              ;;
-          *)
-              ${pkgs.pistol}/bin/pistol "$1"
-              ;;
-      esac
-    '';
-  in {
+  programs.lf = {
     enable = true;
     settings = {
       preview = true;
@@ -26,9 +14,32 @@
       ignorecase = true;
       sixel = true;
     };
-    previewer = {
+    previewer = let
+      previewer = pkgs.writeShellScriptBin "previewer" ''
+        case "$(${pkgs.file}/bin/file -Lb --mime-type -- "$1")" in
+            image/*)
+                ${pkgs.chafa}/bin/chafa -f sixel -s "$2x$3" --animate off --polite on "$1"
+                exit 1
+                ;;
+            *)
+                ${pkgs.pistol}/bin/pistol "$1"
+                ;;
+        esac
+      '';
+    in {
       keybinding = "i";
       source = "${previewer}/bin/previewer";
+    };
+    commands = {
+      open = ''''${{
+        case $(${pkgs.file}/bin/file --mime-type -Lb $f) in
+            text/*) lf -remote "send $id \$$EDITOR \$fx";;
+            *) for f in $fx; do $OPENER "$f" > /dev/null 2> /dev/null & done;;
+        esac
+      }}'';
+    };
+    keybindings = {
+      "<enter>" = "open";
     };
   };
 }
