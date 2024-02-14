e74007ad5e64d68cb7ab3e32f4695852ac01c125
khsaad04 <khsaad0404@gmail.com>
2024-02-14 17:33:21 +0600

    add image preview support to lf

--- home-manager/lf.nix
+++ home-manager/lf.nix
@@ -4,7 +4,22 @@
   ...
 }: {
   xdg.configFile."lf/icons".source = "${inputs.lf-icons}/etc/icons.example";
-  programs.lf = {
+  home.file."./.config/ctpv/config".text = ''
+    set chafasixel
+  '';
+  programs.lf = let
+    previewer = pkgs.writeShellScriptBin "previewer" ''
+      case "$(${pkgs.file}/bin/file -Lb --mime-type -- "$1")" in
+          image/*)
+              ${pkgs.chafa}/bin/chafa -f sixel -s "$2x$3" --animate off --polite on "$1"
+              exit 1
+              ;;
+          *)
+              cat "$1"
+              ;;
+      esac
+    '';
+  in {
     enable = true;
     settings = {
       preview = true;
@@ -12,15 +27,17 @@
       hidden = true;
       icons = true;
       ignorecase = true;
+      sixel = true;
     };
     previewer = {
       keybinding = "i";
-      source = "${pkgs.ctpv}/bin/ctpv";
+      # source = "${pkgs.ctpv}/bin/ctpv";
+      source = "${previewer}/bin/previewer";
     };
-    extraConfig = ''
-      &${pkgs.ctpv}/bin/ctpv -s $id
-      cmd on-quit %${pkgs.ctpv}/bin/ctpv -e $id
-      set cleaner ${pkgs.ctpv}/bin/ctpvclear
-    '';
+    # extraConfig = ''
+    #   &${pkgs.ctpv}/bin/ctpv -s $id
+    #   cmd on-quit %${pkgs.ctpv}/bin/ctpv -e $id
+    #   set cleaner ${pkgs.ctpv}/bin/ctpvclear
+    # '';
   };
 }
