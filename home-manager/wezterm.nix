3f834455d378154ddd42c2682abb683832030d70
khsaad04 <khsaad0404@gmail.com>
2024-02-15 21:50:56 +0600

    refactor wezterm config

--- home-manager/wezterm.nix
+++ home-manager/wezterm.nix
@@ -5,6 +5,15 @@
       return {
         font = wezterm.font("FiraCode Nerd Font"),
         font_size = 11.0,
+        font_rules = {
+          {
+            intensity = "Bold",
+            font = wezterm.font {
+              family = "FiraCode Nerd Font",
+              weight = "Bold",
+            },
+          },
+        },
         color_scheme = "Catppuccin Mocha",
         hide_tab_bar_if_only_one_tab = true,
 
