69c2d3f15aae0120f5e10301b476eeb414d15e49
khsaad04 <khsaad0404@gmail.com>
2024-02-21 20:36:07 +0600

    update nvim treesitter config

--- home-manager/nvim/lua/plugins/treesitter.lua
+++ home-manager/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,66 @@
 return {
-	-- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	event = "BufReadPre",
 	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			-- Add languages to be installed here that you want installed for treesitter
 			ensure_installed = {
+				"c",
 				"lua",
+				"vim",
+				"vimdoc",
+				"query",
 				"python",
 				"rust",
-				"vimdoc",
-				"vim",
 				"regex",
 				"markdown",
 				"bash",
 				"markdown_inline",
 			},
-
 			auto_install = false,
+
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = false,
+			},
+
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<leader>ss", -- set to `false` to disable one of the mappings
+					node_incremental = "<leader>si",
+					scope_incremental = "<leader>sc",
+					node_decremental = "<leader>sd",
+				},
+			},
+
+			textobjects = {
+				select = {
+					enable = true,
+
+					-- Automatically jump forward to textobj, similar to targets.vim
+					lookahead = true,
+
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+						-- You can also use captures from other query groups like `locals.scm`
+						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+					},
+
+					selection_modes = {
+						["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						["@class.outer"] = "<c-v>", -- blockwise
+					},
+					include_surrounding_whitespace = true,
+				},
 			},
 		})
 		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
