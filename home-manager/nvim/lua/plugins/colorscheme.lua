8ff48f3878b43a755a50b733fca24b8f0a13df33
khsaad04 <khsaad0404@gmail.com>
2024-02-22 16:34:07 +0600

    speedup catppuccin

--- home-manager/nvim/lua/plugins/colorscheme.lua
+++ home-manager/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,10 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("catppuccin")
-		require("catppuccin").setup({
-			flavor = "mocha",
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				neotree = true,
-				treesitter = true,
-				noice = true,
-			},
-		})
-	end,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
