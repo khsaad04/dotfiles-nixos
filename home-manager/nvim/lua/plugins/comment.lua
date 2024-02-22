a18ebeaf2cf8925527caceaa3d22b1e3249242a7
khsaad04 <khsaad0404@gmail.com>
2024-02-22 16:13:16 +0600

    major improvements

--- home-manager/nvim/lua/plugins/comment.lua
+++ home-manager/nvim/lua/plugins/comment.lua
@@ -1,5 +1,5 @@
 return {
 	"numToStr/Comment.nvim",
-	event = "BufReadPost",
+	keys = { "gcc", "gc" },
 	opts = {},
 }
--- home-manager/nvim/lua/plugins/telescope.lua
+++ home-manager/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,83 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
-	event = "VeryLazy",
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("telescope.builtin").oldfiles()
+			end,
+			mode = "n",
+			desc = "[?] Find recently opened files",
+		},
+		{
+			"<leader><space>",
+			function()
+				require("telescope.builtin").buffers()
+			end,
+			mode = "n",
+			desc = "[ ] Find existing buffers",
+		},
+		{
+			"<leader>/",
+			function()
+				require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end,
+			mode = "n",
+			desc = "[/] Fuzzily search in current buffer",
+		},
+		{
+			"<leader>gf",
+			function()
+				require("telescope.builtin").git_files()
+			end,
+			mode = "n",
+			desc = "Search [G]it [F]iles",
+		},
+		{
+			"<leader>sf",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+			mode = "n",
+			desc = "[S]earch [F]iles",
+		},
+		{
+			"<leader>sh",
+			function()
+				require("telescope.builtin").help_tags()
+			end,
+			mode = "n",
+			desc = "[S]earch [H]elp",
+		},
+		{
+			"<leader>sw",
+			function()
+				require("telescope.builtin").grep_string()
+			end,
+			mode = "n",
+			desc = "[S]earch current [W]ord",
+		},
+		{
+			"<leader>sg",
+			function()
+				require("telescope.builtin").live_grep()
+			end,
+			mode = "n",
+			desc = "[S]earch by [G]rep",
+		},
+		{
+			"<leader>sD",
+			function()
+				require("telescope.builtin").diagnostics()
+			end,
+			mode = "n",
+			desc = "[S]earch [D]iagnostics",
+		},
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{
@@ -26,38 +102,5 @@ return {
 
 		-- Enable telescope fzf native, if installed
 		pcall(require("telescope").load_extension, "fzf")
-
-		-- See `:help telescope.builtin`
-		vim.keymap.set(
-			"n",
-			"<leader>?",
-			require("telescope.builtin").oldfiles,
-			{ desc = "[?] Find recently opened files" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader><space>",
-			require("telescope.builtin").buffers,
-			{ desc = "[ ] Find existing buffers" }
-		)
-		vim.keymap.set("n", "<leader>/", function()
-			-- You can pass additional configuration to telescope to change theme, layout, etc.
-			require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
-				previewer = false,
-			}))
-		end, { desc = "[/] Fuzzily search in current buffer" })
-
-		vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
-		vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set(
-			"n",
-			"<leader>sw",
-			require("telescope.builtin").grep_string,
-			{ desc = "[S]earch current [W]ord" }
-		)
-		vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sD", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
 	end,
 }
