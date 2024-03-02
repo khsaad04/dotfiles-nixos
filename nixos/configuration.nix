b27d36be1eb7d534f2c587ef82de8eff320ea512
khsaad04 <khsaad0404@gmail.com>
2024-03-02 20:35:55 +0600

    use greetd + gtkgreet instead of sddm(fix)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -50,10 +50,17 @@
   services = {
     xserver.enable = true;
     greetd = {
+      enable = true;
       settings = {
         default_session = let
           swayConfig = pkgs.writeText "greetd-sway-config" ''
+            # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
             exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+            bindsym Mod4+shift+e exec swaynag \
+              -t warning \
+              -m 'What do you want to do?' \
+              -b 'Poweroff' 'systemctl poweroff' \
+              -b 'Reboot' 'systemctl reboot'
           '';
         in {
           command = "${pkgs.swayfx}/bin/sway --config ${swayConfig}";
