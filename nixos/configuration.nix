e43721e5a9196a25ef435dfa614bbc882f7af21c
khsaad04 <khsaad0404@gmail.com>
2024-03-02 16:57:47 +0600

    idk

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -45,23 +45,11 @@
   # Locale
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
   };
 
   services = {
     xserver = {
       enable = true;
-      excludePackages = [pkgs.xterm];
       displayManager.sddm = {
         enable = true;
       };
