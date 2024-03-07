c6550b489dc5baacc08fe97ef7c46d7c26eeaed6
khsaad04 <khsaad0404@gmail.com>
2024-03-07 19:37:49 +0600

    add disko config

---
+++ nixos/disko.nix
@@ -0,0 +1,60 @@
+{
+  disko.devices = {
+    disk = {
+      sda = {
+        type = "disk";
+        device = "/dev/disk/by-diskseq/1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  "/root" = {
+                    mountOptions = ["compress=zstd" "noatime"];
+                    mountpoint = "/";
+                  };
+
+                  "/home" = {
+                    mountOptions = ["compress=zstd" "noatime"];
+                    mountpoint = "/home";
+                  };
+
+                  "/nix" = {
+                    mountOptions = ["compress=zstd" "noatime"];
+                    mountpoint = "/nix/store";
+                  };
+
+                  "/swap" = {
+                    mountOptions = ["noatime"];
+                    mountpoint = "/.swapvol";
+                    swap = {
+                      swapfile.size = "12288M";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
