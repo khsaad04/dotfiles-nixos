ddc373561f0b57da73a9e6bee5c558de8b49a193
khsaad04 <khsaad0404@gmail.com>
2024-03-07 15:03:31 +0600

    move extra-substituters to nixos configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -38,6 +38,10 @@
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
       trusted-users = ["khsaad"];
+      extra-substituters = ["https://nix-community.cachix.org"];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 
