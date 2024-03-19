8e6c329bce7b8d61124e59fa8dbf9232021c53a8
khsaad04 <khsaad0404@gmail.com>
2024-03-19 22:08:14 +0600

    add systemd-boot configuration limit

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -1,45 +1,41 @@
-{ lib
-, pkgs
+{ pkgs
 , ...
 }: {
   nix = {
     settings = {
-      warn-dirty = lib.mkDefault false;
-      auto-optimise-store = lib.mkDefault true;
-      experimental-features = lib.mkDefault [ "nix-command" "flakes" ];
-      trusted-users = lib.mkDefault [ "khsaad" ];
-      extra-substituters = lib.mkDefault [ "https://nix-community.cachix.org" ];
-      extra-trusted-public-keys = lib.mkDefault [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
+      warn-dirty = false;
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [ "khsaad" ];
     };
     gc = {
-      automatic = lib.mkDefault true;
-      dates = lib.mkDefault "weekly";
-      options = lib.mkDefault "--delete-older-than 7d";
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
     };
   };
 
   boot = {
-    kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
+    kernelPackages = pkgs.linuxPackages_zen;
     loader = {
-      systemd-boot.enable = lib.mkDefault true;
-      efi.canTouchEfiVariables = lib.mkDefault true;
+      systemd-boot.enable = true;
+      systemd-boot.configurationLimit = 10;
+      efi.canTouchEfiVariables = true;
     };
   };
 
   security = {
-    polkit.enable = lib.mkDefault true;
-    rtkit.enable = lib.mkDefault true;
+    polkit.enable = true;
+    rtkit.enable = true;
   };
 
   services = {
     pipewire = {
-      alsa.enable = lib.mkDefault true;
-      alsa.support32Bit = lib.mkDefault true;
-      pulse.enable = lib.mkDefault true;
-      jack.enable = lib.mkDefault true;
-      wireplumber.enable = lib.mkDefault true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+      wireplumber.enable = true;
     };
   };
 }
