12ed23e968fc100b55013e8aa75c9b9d2398e79e
khsaad04 <khsaad0404@gmail.com>
2024-03-22 21:10:01 +0600

    Add vim-fugitive plugin to nvim

--- modules/home-manager/nvim/src/lua/plugins/git.lua
+++ modules/home-manager/nvim/src/lua/plugins/git.lua
@@ -1,5 +1,4 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = "BufReadPre",
-  opts = {},
+  { "lewis6991/gitsigns.nvim", event = "BufReadPre", opts = {} },
+  { "tpope/vim-fugitive", event = "BufReadPre" },
 }
