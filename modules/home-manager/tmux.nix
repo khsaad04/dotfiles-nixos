f78cbcbac2190f9e88788cb7b68687e62de24e50
khsaad04 <khsaad0404@gmail.com>
2024-04-13 16:55:17 +0600

    Simplify tmux config

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -28,86 +28,31 @@ in
     programs.tmux = {
       enable = cfg.enable;
       extraConfig = ''
-        # Fix colors for terminal
-        set -g default-terminal "tmux-256color"
-        set -ag terminal-overrides ",xterm-256color:RGB"
-
-        # Setting ctrl + a as prefix
+        # Change prefix
         set -g prefix C-space
-        unbind C-b
+
+        # Increase tmux messages display duration from 750ms to 2s
+        set -g display-time 2000
+
+        # Vi key bindings in tmux command prompt (prefix + :)
+        set -g status-keys vi
+
+        # Reloading .conf file
+        bind r source-file ~/.config/tmux/tmux.conf \; display "Config file has be reloaded "
+        
+        # Disable confirmation prompt
+        bind-key -N "Kill the current window" & kill-window
+        bind-key -N "Kill the current pane" x kill-pane
+
+        # Clipbaord Integration
+        set -g set-clipboard on
 
         # Start indexing windows at 1
         set -g base-index 1
 
-        # Renumber windows when they are closed
-        set -g renumber-windows on
-
-        # Enable mouse support for TMUX
-        set -g mouse on
-
-        # Make delay shorter
-        set -sg escape-time 0
-
-        # Making things sense
-        set -g history-limit 50000
-        set -g focus-events on
-        set-window-option -g mode-keys vi
-
-        # Reloading .conf file
-        bind r source-file ~/.config/tmux/tmux.conf \; display "Config file has be reloaded "
-
-        # Split Windows
-        bind -n M-h split-window -h
-        bind -n M-v split-window -v
-
-        # Close Windows
-        bind -n M-x kill-pane
-
-        # Panes Navigation
-        bind -n M-H select-pane -L
-        bind -n M-J select-pane -D
-        bind -n M-K select-pane -U
-        bind -n M-L select-pane -R
-
-        # Swap Pane Navigation:
-        bind -n M-] swap-pane -D
-        bind -n M-[ swap-pane -U
-
-        # Window Manipulation
-        bind -n -r C-t new-window
-        bind -n -r M-. next-window
-
-        bind -n -r M-, previous-window
-        # Rename Windows
-        bind -n F2 command-prompt -p " 󰑕 Rename window:" "rename-window '%%'"
-
-        # Resize Windows
-        bind -n M-Left  resize-pane -L 2
-        bind -n M-Right resize-pane -R 2
-        bind -n M-Down  resize-pane -D 2
-        bind -n M-Up    resize-pane -U 2
-
-        # Navigation: Windows
-        bind -n M-0 select-window -t 0
-        bind -n M-1 select-window -t 1
-        bind -n M-2 select-window -t 2
-        bind -n M-3 select-window -t 3
-        bind -n M-4 select-window -t 4
-        bind -n M-5 select-window -t 5
-        bind -n M-6 select-window -t 6
-        bind -n M-7 select-window -t 7
-        bind -n M-8 select-window -t 8
-        bind -n M-9 select-window -t 9
-
-        # Break a pane and move it to a new window
-        bind -n M-q break-pane \; display "Moved pane to a new window "
-
         # Tmux-sessionizer
         bind C-o display-popup -E "${pkgs.tmux-sessionizer}/bin/tms"
         bind C-j display-popup -E "${pkgs.tmux-sessionizer}/bin/tms switch"
-
-        # Clipbaord Integration
-        set -g set-clipboard on
 
         # Status bar
         set -g status-interval 1
