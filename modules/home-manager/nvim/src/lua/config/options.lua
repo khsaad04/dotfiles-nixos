17da832f9bf71d0fefb91045ce6016bf1e2632f9
khsaad04 <khsaad0404@gmail.com>
2024-04-20 16:22:59 +0600

    Update neovim config

--- modules/home-manager/nvim/src/lua/config/options.lua
+++ modules/home-manager/nvim/src/lua/config/options.lua
@@ -5,35 +5,25 @@ local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
-
+opt.showmode = false
 opt.splitbelow = true
 opt.splitright = true
-
 opt.wrap = false
-
 opt.mouse = "a"
-
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
-
 opt.scrolloff = 999
 opt.virtualedit = "block"
-
 opt.clipboard = "unnamedplus"
-
 opt.undofile = true
-
 opt.ignorecase = true
 opt.smartcase = true
-
+opt.inccommand = "split"
 opt.signcolumn = "yes:1"
-
-opt.updatetime = 100
+opt.updatetime = 250
 opt.timeoutlen = 500
-
 opt.completeopt = "menuone,noselect"
-
 opt.termguicolors = true
 opt.cmdheight = 1
