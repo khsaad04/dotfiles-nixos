fc5d5fad5b5d3ff4a144bc75f5adb9c8429c27ed
khsaad04 <khsaad0404@gmail.com>
2024-04-24 21:06:25 +0600

    neovim: Change gitsigns icons

--- modules/home-manager/nvim/src/lua/plugins/git.lua
+++ modules/home-manager/nvim/src/lua/plugins/git.lua
@@ -1,4 +1,16 @@
 return {
-  { "lewis6991/gitsigns.nvim", event = "VeryLazy", opts = {} },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "VeryLazy",
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    },
+  },
   { "tpope/vim-fugitive", event = "VeryLazy" },
 }
