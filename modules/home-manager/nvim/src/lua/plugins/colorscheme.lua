834b67d4b84d4d5b2ac22b6024d4ee9094e88068
khsaad04 <khsaad0404@gmail.com>
2024-04-30 12:59:06 +0600

    nvim: Update a few plugin configs

--- modules/home-manager/nvim/src/lua/plugins/colorscheme.lua
+++ modules/home-manager/nvim/src/lua/plugins/colorscheme.lua
@@ -4,44 +4,8 @@ return {
     name = "catppuccin",
     lazy = false,
     priority = 1000,
+    opts = {},
     config = function()
-      require("catppuccin").setup({
-        default_integrations = false,
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          indent_blankline = {
-            enabled = true,
-            scope_color = "",
-            colored_indent_levels = false,
-          },
-          native_lsp = {
-            enabled = true,
-            virtual_text = {
-              errors = { "italic" },
-              hints = { "italic" },
-              warnings = { "italic" },
-              information = { "italic" },
-            },
-            underlines = {
-              errors = { "underline" },
-              hints = { "underline" },
-              warnings = { "underline" },
-              information = { "underline" },
-            },
-            inlay_hints = {
-              background = true,
-            },
-          },
-          fidget = true,
-          neotree = true,
-          telescope = { enabled = true },
-          treesitter = true,
-          treesitter_context = true,
-          semantic_tokens = true,
-          which_key = true,
-        },
-      })
       vim.cmd.colorscheme("catppuccin")
     end,
   },
--- modules/home-manager/nvim/src/lua/plugins/lualine.lua
+++ modules/home-manager/nvim/src/lua/plugins/lualine.lua
@@ -2,13 +2,10 @@ return {
   "nvim-lualine/lualine.nvim",
   lazy = false,
   priority = 999,
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "auto",
-        component_separators = "",
-        section_separators = "",
-      },
-    })
-  end,
+  opts = {
+    options = {
+      component_separators = "",
+      section_separators = "",
+    },
+  },
 }
