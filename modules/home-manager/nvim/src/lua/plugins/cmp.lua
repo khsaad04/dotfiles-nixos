a3aa0d10c3cecac73c20206f71818f0059a2ac45
khsaad04 <khsaad0404@gmail.com>
2024-05-31 18:22:41 +0600

    nvim: Add friendly snippets

--- modules/home-manager/nvim/src/lua/plugins/cmp.lua
+++ modules/home-manager/nvim/src/lua/plugins/cmp.lua
@@ -2,7 +2,7 @@ return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    "L3MON4D3/LuaSnip",
+    { "L3MON4D3/LuaSnip", dependencies = "rafamadriz/friendly-snippets" },
     "saadparwaiz1/cmp_luasnip",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
