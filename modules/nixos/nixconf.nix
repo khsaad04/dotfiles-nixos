9bd7ca104fdf92da1cd8eb1cbfaaceefb4d7fc92
khsaad04 <khsaad0404@gmail.com>
2024-08-10 15:32:00 +0600

    fix: disable nix channels in nix conf

--- modules/nixos/nixconf.nix
+++ modules/nixos/nixconf.nix
@@ -8,7 +8,13 @@
   options.local.nixConfig.enable = lib.mkEnableOption "Enable nix configurations";
   config = lib.mkIf config.local.nixConfig.enable {
     nix = {
+      channel.enable = false;
+      registry = (lib.mapAttrs (_: flake: { inherit flake; })) (
+        (lib.filterAttrs (_: lib.isType "flake")) inputs
+      );
+      nixPath = [ "/etc/nix/path" ];
       settings = {
+        flake-registry = "";
         warn-dirty = false;
         auto-optimise-store = true;
         experimental-features = [
@@ -27,11 +33,6 @@
       };
     };
 
-    nix.registry = (lib.mapAttrs (_: flake: { inherit flake; })) (
-      (lib.filterAttrs (_: lib.isType "flake")) inputs
-    );
-
-    nix.nixPath = [ "/etc/nix/path" ];
     environment.etc = lib.mapAttrs' (name: value: {
       name = "nix/path/${name}";
       value.source = value.flake;
