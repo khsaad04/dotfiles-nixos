2cdbebe440dbf42d32f454ca5d8870e6d0fb94d3
khsaad04 <khsaad0404@gmail.com>
2024-08-11 21:21:49 +0600

    fix(Waybar): remove onChange script that doesn't really work

--- modules/home-manager/waybar/default.nix
+++ modules/home-manager/waybar/default.nix
@@ -14,28 +14,18 @@ let
     "@spacing" = "2px";
     "@margin" = "2px 2px";
   };
-  onChange = ''
-    pkill waybar && waybar
-  '';
 in
 {
   options.local.programs.waybar.enable = lib.mkEnableOption "Enable waybar configuration";
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.playerctl ];
-    xdg.configFile."waybar/config" = {
-      inherit onChange;
-      source = "${./config.json}";
-    };
-    xdg.configFile."waybar/colors.css" = {
-      inherit onChange;
-      source = "${config.programs.matugen.theme.files}/.config/waybar/colors.css";
-    };
-    xdg.configFile."waybar/style.css" = {
-      inherit onChange;
-      text = builtins.replaceStrings (builtins.attrNames css_vars) (builtins.attrValues css_vars) ''
-        ${builtins.readFile ./style.css}
-      '';
-    };
+    xdg.configFile."waybar/config".source = "${./config.json}";
+    xdg.configFile."waybar/colors.css".source = "${config.programs.matugen.theme.files}/.config/waybar/colors.css";
+    xdg.configFile."waybar/style.css".text =
+      builtins.replaceStrings (builtins.attrNames css_vars) (builtins.attrValues css_vars)
+        ''
+          ${builtins.readFile ./style.css}
+        '';
     programs.waybar = {
       inherit (cfg) enable;
     };
