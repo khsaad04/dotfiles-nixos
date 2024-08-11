c7ff95acf91a262d28160f85ff685cb6530628d0
khsaad04 <khsaad0404@gmail.com>
2024-08-11 11:51:43 +0600

    refactor: Nix conf

--- modules/nixos/nixconf.nix
+++ modules/nixos/nixconf.nix
@@ -7,35 +7,33 @@
 {
   options.local.nixConfig.enable = lib.mkEnableOption "Enable nix configurations";
   config = lib.mkIf config.local.nixConfig.enable {
-    nix = {
-      channel.enable = false;
-      registry = (lib.mapAttrs (_: flake: { inherit flake; })) (
-        (lib.filterAttrs (_: lib.isType "flake")) inputs
-      );
-      nixPath = [ "/etc/nix/path" ];
-      settings = {
-        flake-registry = "";
-        warn-dirty = false;
-        auto-optimise-store = true;
-        experimental-features = [
-          "nix-command"
-          "flakes"
-        ];
-        trusted-users = [ "khsaad" ];
-        substituters = [
-          "https://hyprland.cachix.org"
-          "https://cosmic.cachix.org/"
-        ];
-        trusted-public-keys = [
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-        ];
+    nix =
+      let
+        flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+      in
+      {
+        channel.enable = false;
+        registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+        nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+        settings = {
+          flake-registry = "";
+          nix-path = config.nix.nixPath;
+          warn-dirty = false;
+          auto-optimise-store = true;
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          trusted-users = [ "khsaad" ];
+          substituters = [
+            "https://hyprland.cachix.org"
+            "https://cosmic.cachix.org/"
+          ];
+          trusted-public-keys = [
+            "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+          ];
+        };
       };
-    };
-
-    environment.etc = lib.mapAttrs' (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    }) config.nix.registry;
   };
 }
