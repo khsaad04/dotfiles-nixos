7132589e12e473da46790263a2b037de5d085176
khsaad04 <khsaad0404@gmail.com>
2024-08-17 17:09:26 +0600

    feat(Nix): Add substituters and enable keeping GC roots

--- modules/nixos/nixconf.nix
+++ modules/nixos/nixconf.nix
@@ -1,29 +1,37 @@
+{ config, inputs, ... }:
 {
-  lib,
-  config,
-  inputs,
-  ...
-}:
-{
-  options.local.nixConfig.enable = lib.mkEnableOption "Enable nix configurations";
-  config = lib.mkIf config.local.nixConfig.enable {
-    nix = {
-      channel.enable = false;
-      registry.nixpkgs.flake = inputs.nixpkgs;
-      nixPath = [ "nixpkgs=flake:nixpkgs" ];
-      settings = {
-        flake-registry = "";
-        nix-path = config.nix.nixPath;
-        warn-dirty = false;
-        auto-optimise-store = true;
-        experimental-features = [
-          "nix-command"
-          "flakes"
-        ];
-        trusted-users = [ "khsaad" ];
-        substituters = [ "https://hyprland.cachix.org" ];
-        trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-      };
+  nix = {
+    channel.enable = false;
+    registry.nixpkgs.flake = inputs.nixpkgs;
+    nixPath = [ "nixpkgs=flake:nixpkgs" ];
+    settings = {
+      flake-registry = "";
+      nix-path = config.nix.nixPath;
+      warn-dirty = false;
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+
+      # for direnv GC roots
+      keep-derivations = true;
+      keep-outputs = true;
+
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+      substituters = [
+        "https://cache.nixos.org?priority=10"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 }
