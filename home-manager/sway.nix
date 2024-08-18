0611f76419826aae838f9dc4d9256f9ebd36479c
khsaad04 <khsaad0404@gmail.com>
2024-08-18 12:41:54 +0600

    refactor(Sway): Manually create the config file

--- home-manager/sway.nix
+++ home-manager/sway.nix
@@ -1,9 +1,4 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}:
+{ pkgs, config, ... }:
 {
   home.packages = [
     pkgs.networkmanagerapplet
@@ -16,73 +11,162 @@
     enable = true;
     package = pkgs.swayfx;
     checkConfig = false;
-    config = {
-      defaultWorkspace = "workspace number 1";
-      startup = [
-        { command = "wl-clipboard &"; }
-        { command = "wl-clip-persist --clipboard regular &"; }
-        { command = "nm-applet --indicator &"; }
-        { command = "firefox &"; }
-        { command = "autotiling-rs &"; }
-      ];
-      fonts = {
-        names = [ "${config.local.theming.font}" ];
-        style = "Regular";
-        size = 10.0;
-      };
-      seat."*" = {
-        xcursor_theme = "${config.local.theming.cursor.name} ${toString config.local.theming.cursor.size}";
-      };
-      gaps = {
-        inner = 2;
-        outer = 2;
-      };
-      output = {
-        "*" = {
-          bg = "${config.local.theming.wallpaper} fill";
-        };
-      };
-      modifier = "Mod4";
-      left = "h";
-      down = "j";
-      up = "k";
-      right = "l";
-      terminal = "wezterm";
-      menu = "wofi -I --show drun | xargs swaymsg exec --";
-      input = {
-        "type:pointer" = {
-          accel_profile = "flat";
-          pointer_accel = "0";
-        };
-        "type:keyboard" = {
-          xkb_options = "caps:escape";
-        };
-      };
-      keybindings =
-        let
-          cfg = config.wayland.windowManager.sway;
-          inherit (cfg.config) modifier;
-        in
-        lib.mkOptionDefault {
-          "${modifier}+space" = "exec ${cfg.config.menu}";
-          "${modifier}+q" = "kill";
-          "${modifier}+x" = "exec powermenu";
-          "Print" = "exec grimblast --notify copysave";
-          "Shift+Print" = "exec grimblast --notify copysave area";
-          "XF86AudioRaiseVolume" = "exec changevolume up";
-          "XF86AudioLowerVolume" = "exec changevolume down";
-          "XF86AudioMute" = "exec changevolume mute";
-        };
-      assigns = {
-        "2" = [ { app_id = "firefox"; } ];
-      };
-      bars = [ ];
-    };
+    config = null;
     extraConfig = ''
+      exec wl-clipboard &
+      exec wl-clip-persist --clipboard regular &
+      exec nm-applet --indicator &
+      exec firefox &
+      exec autotiling-rs &
+
+      ### Variables
+      set $mod Mod4
+
+      set $left h
+      set $down j
+      set $up k
+      set $right l
+
+      set $term wezterm
+
+      set $menu wofi -I --show drun | xargs swaymsg exec --
+
+      ### Appearance
       corner_radius 8
-      blur enable
+      gaps inner 2
+      gaps outer 2
       default_border none
       smart_gaps on
+      blur on
+
+      ### Assign
+      assign [app_id="firefox"] 2
+
+      ### Output configuration
+      output * bg ${config.local.theming.wallpaper} fill
+
+      ### Input configuration
+      input "type:pointer" {
+        accel_profile flat
+        pointer_accel 0
+      }
+      input "type:keyboard" {
+        xkb_options caps:escape
+      }
+
+      seat "*" {
+          xcursor_theme ${config.local.theming.cursor.name} ${toString config.local.theming.cursor.size}
+      }
+
+      ### Key bindings
+      #
+      # Basics:
+      #
+          bindsym $mod+Return exec $term
+          bindsym $mod+Shift+q kill
+          bindsym $mod+d exec $menu
+          floating_modifier $mod normal
+          bindsym $mod+Shift+c reload
+          bindsym $mod+Shift+e exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' 'swaymsg exit'
+          bindsym $mod+x exec powermenu
+      #
+      # Moving around:
+      #
+          bindsym $mod+$left focus left
+          bindsym $mod+$down focus down
+          bindsym $mod+$up focus up
+          bindsym $mod+$right focus right
+
+          bindsym $mod+Left focus left
+          bindsym $mod+Down focus down
+          bindsym $mod+Up focus up
+          bindsym $mod+Right focus right
+
+          bindsym $mod+Shift+$left move left
+          bindsym $mod+Shift+$down move down
+          bindsym $mod+Shift+$up move up
+          bindsym $mod+Shift+$right move right
+
+          bindsym $mod+Shift+Left move left
+          bindsym $mod+Shift+Down move down
+          bindsym $mod+Shift+Up move up
+          bindsym $mod+Shift+Right move right
+      #
+      # Workspaces:
+      #
+          # Switch to workspace
+          bindsym $mod+1 workspace number 1
+          bindsym $mod+2 workspace number 2
+          bindsym $mod+3 workspace number 3
+          bindsym $mod+4 workspace number 4
+          bindsym $mod+5 workspace number 5
+          bindsym $mod+6 workspace number 6
+          bindsym $mod+7 workspace number 7
+          bindsym $mod+8 workspace number 8
+          bindsym $mod+9 workspace number 9
+          bindsym $mod+0 workspace number 10
+
+          # Move focused container to workspace
+          bindsym $mod+Shift+1 move container to workspace number 1
+          bindsym $mod+Shift+2 move container to workspace number 2
+          bindsym $mod+Shift+3 move container to workspace number 3
+          bindsym $mod+Shift+4 move container to workspace number 4
+          bindsym $mod+Shift+5 move container to workspace number 5
+          bindsym $mod+Shift+6 move container to workspace number 6
+          bindsym $mod+Shift+7 move container to workspace number 7
+          bindsym $mod+Shift+8 move container to workspace number 8
+          bindsym $mod+Shift+9 move container to workspace number 9
+          bindsym $mod+Shift+0 move container to workspace number 10
+      #
+      # Layout stuff:
+      #
+          bindsym $mod+b splith
+          bindsym $mod+v splitv
+
+          bindsym $mod+s layout stacking
+          bindsym $mod+w layout tabbed
+          bindsym $mod+e layout toggle split
+
+          bindsym $mod+f fullscreen
+          bindsym $mod+Shift+space floating toggle
+          bindsym $mod+space focus mode_toggle
+          bindsym $mod+a focus parent
+      #
+      # Scratchpad:
+      #
+          bindsym $mod+Shift+minus move scratchpad
+          bindsym $mod+minus scratchpad show
+      #
+      # Resizing containers:
+      #
+      mode "resize" {
+          bindsym $left resize shrink width 10px
+          bindsym $down resize grow height 10px
+          bindsym $up resize shrink height 10px
+          bindsym $right resize grow width 10px
+
+          bindsym Left resize shrink width 10px
+          bindsym Down resize grow height 10px
+          bindsym Up resize shrink height 10px
+          bindsym Right resize grow width 10px
+
+          bindsym Return mode "default"
+          bindsym Escape mode "default"
+      }
+      bindsym $mod+r mode "resize"
+
+      #
+      # Screenshot and Volume
+      #
+          bindsym Print exec grimblast --notify copysave
+          bindsym Shift+Print exec grimblast --notify copysave area
+          bindsym XF86AudioLowerVolume exec changevolume down
+          bindsym XF86AudioMute exec changevolume mute
+          bindsym XF86AudioRaiseVolume exec changevolume up
+
+      #
+      # Status Bar:
+      #
       bar {
         swaybar_command waybar
       }
