ea3771777308513a22569799390754a25640b1f9
khsaad04 <khsaad0404@gmail.com>
2024-08-22 14:32:00 +0600

    feat(Quickshell): Add onChange script

--- home-manager/quickshell/default.nix
+++ home-manager/quickshell/default.nix
@@ -13,10 +13,16 @@ let
     cp -r ${./src}/* $out 
     cp ${config.programs.matugen.theme.files}/.config/quickshell/Colors.qml $out
   '';
+  onChange = ''
+    ${pkgs.procps}/bin/pgrep quickshell && ${
+      inputs.hyprland.packages.${pkgs.hostPlatform.system}.default
+    }/bin/hyprctl dispatch exec "pkill quickshell;quickshell &" || true
+  '';
 in
 {
   home.packages = [ inputs.quickshell.packages.${pkgs.system}.default ];
   xdg.configFile."quickshell" = {
+    inherit onChange;
     source = "${configs}";
     recursive = true;
   };
