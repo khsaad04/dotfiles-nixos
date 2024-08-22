c32c85f7ea82063f0d45823681c5372d0a7cb852
khsaad04 <khsaad0404@gmail.com>
2024-08-22 11:48:23 +0600

    feat: Add multilingual input method

--- home-manager/profiles/khsaad/home.nix
+++ home-manager/profiles/khsaad/home.nix
@@ -34,6 +34,16 @@
     inputs.self.packages.${pkgs.system}.formatter
   ];
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-openbangla-keyboard ];
+  };
+  home.sessionVariables = {
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+  };
+
   news.display = "silent";
   dconf.enable = true;
 }
