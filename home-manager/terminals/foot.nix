6d205f1335170929142e1edf2d8d571f0880722f
khsaad04 <khsaad0404@gmail.com>
2024-08-23 23:03:06 +0600

    refactor(Foot): remove comments

--- home-manager/terminals/foot.nix
+++ home-manager/terminals/foot.nix
@@ -9,28 +9,28 @@ in
     settings = {
       main = {
         font = "${config.local.theming.font.name}:size=${toString config.local.theming.font.size},Symbols Nerd Font";
-        pad = "10x10";
+        pad = "4x4 center";
       };
       colors = {
         alpha = 0.95;
         background = strip matu.surface;
         foreground = strip matu.on_surface;
-        regular0 = "${strip clr.base0}"; # Surface 1
-        regular1 = "${strip clr.base1}"; # red
-        regular2 = "${strip clr.base2}"; # green
-        regular3 = "${strip clr.base3}"; # yellow
-        regular4 = "${strip clr.base4}"; # blue
-        regular5 = "${strip clr.base5}"; # pink
-        regular6 = "${strip clr.base6}"; # teal
-        regular7 = "${strip clr.base7}"; # Subtext 1
-        bright0 = "${strip clr.base8}"; # Surface 2
-        bright1 = "${strip clr.base9}"; # red
-        bright2 = "${strip clr.base10}"; # green
-        bright3 = "${strip clr.base11}"; # yellow
-        bright4 = "${strip clr.base12}"; # blue
-        bright5 = "${strip clr.base13}"; # pink
-        bright6 = "${strip clr.base14}"; # teal
-        bright7 = "${strip clr.base15}"; # Subtext 0
+        regular0 = "${strip clr.base0}";
+        regular1 = "${strip clr.base1}";
+        regular2 = "${strip clr.base2}";
+        regular3 = "${strip clr.base3}";
+        regular4 = "${strip clr.base4}";
+        regular5 = "${strip clr.base5}";
+        regular6 = "${strip clr.base6}";
+        regular7 = "${strip clr.base7}";
+        bright0 = "${strip clr.base8}";
+        bright1 = "${strip clr.base9}";
+        bright2 = "${strip clr.base10}";
+        bright3 = "${strip clr.base11}";
+        bright4 = "${strip clr.base12}";
+        bright5 = "${strip clr.base13}";
+        bright6 = "${strip clr.base14}";
+        bright7 = "${strip clr.base15}";
       };
     };
   };
