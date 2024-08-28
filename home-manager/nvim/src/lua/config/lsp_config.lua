90e5993d6b2a278c6e5849ae3279a827b5dae38c
khsaad04 <khsaad0404@gmail.com>
2024-08-28 23:19:21 +0600

    feat(Nvim): Refactor entire lsp config

---
+++ home-manager/nvim/src/lua/config/lsp_config.lua
@@ -0,0 +1,17 @@
+return {
+    ["lua_ls"] = {
+        settings = {
+            Lua = {
+                diagnostics = {
+                    globals = { "vim" },
+                },
+            },
+        },
+    },
+    ["nil_ls"] = {},
+    ["rust_analyzer"] = {},
+    ["zls"] = {},
+    ["clangd"] = {},
+    ["ruff"] = {},
+    ["pylsp"] = {},
+}
--- home-manager/nvim/src/lua/plugins/lsp.lua
+++ home-manager/nvim/src/lua/plugins/lsp.lua
@@ -3,104 +3,48 @@ return {
     ft = { "python", "rust", "nix", "lua", "zig", "c" },
     config = function()
         local lspconfig = require("lspconfig")
-        local on_attach = function(_, bufnr)
-            local nmap = function(keys, func, desc)
-                if desc then
-                    desc = "LSP: " .. desc
-                end
-                vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-            end
-
-            nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-            nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-            nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-            nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-            nmap("<leader>k", vim.lsp.buf.signature_help, "Signature Documentation")
-            nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-            nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-            nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-            nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-            nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-        end
-
-        -- Setup neovim lua configuration
-        -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-        lspconfig.lua_ls.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                },
-            },
-        })
-
-        lspconfig.nil_ls.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-
-        lspconfig.rust_analyzer.setup({
-            on_attach = on_attach,
+        lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+            on_attach = function(_, bufnr)
+                local nmap = function(keys, func, desc)
+                    if desc then
+                        desc = "LSP: " .. desc
+                    end
+                    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+                end
+                nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+                nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+                nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+                nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+                nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+                nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+                nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+                nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+                nmap("<space>e", vim.diagnostic.open_float, "Open diagnostic float")
+            end,
             capabilities = capabilities,
         })
 
-        lspconfig.zls.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-
-        lspconfig.clangd.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-
-        lspconfig.pylsp.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
+        for k, v in pairs(require("config.lsp_config")) do
+            lspconfig[k].setup(v)
+        end
 
         -- Diagnostics
-        vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-        vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
-
         local signs = {
             { name = "DiagnosticSignError", text = "" },
             { name = "DiagnosticSignWarn", text = "" },
             { name = "DiagnosticSignHint", text = "" },
             { name = "DiagnosticSignInfo", text = "" },
         }
-
         for _, sign in ipairs(signs) do
             vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
         end
-
-        local config = {
-            underline = true,
-            virtual_text = true,
+        vim.diagnostic.config({
             signs = {
                 active = signs,
             },
-            update_in_insert = true,
             severity_sort = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-                suffix = "",
-            },
-        }
-
-        vim.diagnostic.config(config)
+        })
     end,
 }
