90a13ce2cefc57c16d091b037f333df6ed580ef1
khsaad04 <khsaad0404@gmail.com>
2024-08-29 12:54:35 +0600

    feat: Update formatter

--- packages/formatter/default.nix
+++ packages/formatter/default.nix
@@ -16,8 +16,14 @@ writeShellApplication {
     fd
   ];
   text = ''
-    fd '.*\.nix' . -x statix fix -- {} \;
-    fd '.*\.nix' . -X deadnix -e -- {} \; -X nixfmt {} \;
-    fd '.*\.lua' . -X stylua --indent-type Spaces --indent-width 4 {} \;
+    if [ -z "''${1:-""}" ] || [ "$1" == "." ]; then
+      fd '.*\.nix' . -x statix fix -- {} \;
+      fd '.*\.nix' . -X deadnix -e -- {} \; -X nixfmt {} \;
+      fd '.*\.lua' . -X stylua --indent-type Spaces --indent-width 4 {} \;
+    else
+      statix fix -- "$1"
+      deadnix -e "$1"
+      nixfmt "$1"
+    fi
   '';
 }
