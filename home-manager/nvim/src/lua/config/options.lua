dc1065a4dc62f81f960abe048eb6076c316c5a12
khsaad04 <khsaad0404@gmail.com>
2024-09-15 17:05:05 +0600

    feat(Neovim): Change some options

--- home-manager/nvim/src/lua/config/options.lua
+++ home-manager/nvim/src/lua/config/options.lua
@@ -2,6 +2,10 @@ local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
 opt.smartindent = true
 opt.showmode = false
 opt.cmdheight = 1
