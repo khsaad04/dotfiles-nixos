7990ecd9de1c873335130dac0de575003dfda84d
khsaad04 <khsaad0404@gmail.com>
2024-10-03 22:25:24 +0600

    feat(nvim): Update compile-mode plugin config

--- home-manager/nvim/src/lua/plugins/compile-mode.lua
+++ home-manager/nvim/src/lua/plugins/compile-mode.lua
@@ -2,9 +2,13 @@ return {
     "ej-shafran/compile-mode.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
+        "m00qek/baleia.nvim",
     },
     config = function()
-        vim.keymap.set("n", "<C-n>", "<cmd>NextError<cr>", { desc = "Go to [N]ext error in compile mode" })
-        vim.keymap.set("n", "<C-p>", "<cmd>PrevError<cr>", { desc = "Go to [P]revious error in compile mode" })
+        vim.keymap.set("n", "<leader>n", "<cmd>NextError<cr>", { desc = "Go to [N]ext error in compile mode" })
+        vim.keymap.set("n", "<leader>p", "<cmd>PrevError<cr>", { desc = "Go to [P]revious error in compile mode" })
+        vim.g.compile_mode = {
+            baleia_setup = true,
+        }
     end,
 }
