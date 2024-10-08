7db9051d32b6a188a6c5361e97679d69b74a4a8b
khsaad04 <khsaad0404@gmail.com>
2024-10-08 23:26:15 +0600

    feat(tofi): Adjust height

--- home-manager/tofi.nix
+++ home-manager/tofi.nix
@@ -7,7 +7,7 @@ in
     settings = {
       anchor = "bottom";
       width = "100%";
-      height = 25;
+      height = 22;
       horizontal = true;
       font = config.local.theming.font.name;
       font-size = config.local.theming.font.size;
