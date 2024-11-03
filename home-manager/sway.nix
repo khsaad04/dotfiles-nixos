b4ff6cd66fa95841f02b6d6fd2bdb0342b363dd3
khsaad04 <khsaad0404@gmail.com>
2024-11-03 11:11:55 +0600

    refactor(sway): Clean up

--- home-manager/sway.nix
+++ home-manager/sway.nix
@@ -19,26 +19,30 @@ in
     checkConfig = false;
     systemd.xdgAutostart = true;
     extraConfig = ''
-      # Replaced with systemd services
-      # exec nm-applet --indicator &
-      # exec dunst &
-      exec firefox &
-      exec foot --server &
-      exec wl-clipboard &
-      exec wl-clip-persist --clipboard regular &
-
-      font pango:${config.local.theming.font.name} ${toString config.local.theming.font.size} 
+      ### Exec stuff
+      bar swaybar_command waybar
+      exec {
+        firefox &
+        foot --server &
+        # Replaced with systemd services
+        # nm-applet --indicator &
+        # dunst &
+        wl-clipboard &
+        wl-clip-persist --clipboard regular &
+      }
 
       ### Variables
-      set $mod Mod4
+      set {
+        $mod Mod4
 
-      set $left h
-      set $down j
-      set $up k
-      set $right l
+        $left h
+        $down j
+        $up k
+        $right l
 
-      set $term footclient
-      set $menu tofi-drun --terminal=footclient | xargs -r swaymsg exec -- 
+        $term footclient
+        $menu tofi-drun --terminal=footclient | xargs -r swaymsg exec -- 
+      }
 
       ### Appearance
       # class                   border      background  text        indicator   child_border
@@ -50,15 +54,16 @@ in
       client.urgent             ${matu.surface}     ${matu.surface}     ${matu.error}     ${matu.surface}     ${matu.surface}
       client.placeholder        ${matu.surface}     ${matu.surface}     ${matu.error}     ${matu.surface}     ${matu.surface}
 
-      # corner_radius 8
-      gaps inner 2
-      gaps outer 2
+      font pango:${config.local.theming.font.name} ${toString (config.local.theming.font.size - 2)} 
+      gaps {
+        inner 2
+        outer 2
+      }
       default_border none
       default_floating_border none
       smart_gaps on
-      # blur on
 
-      ### Assign
+      ### Window Rules
       assign [app_id="firefox"] 2
 
       ### Output configuration
@@ -69,6 +74,7 @@ in
         accel_profile flat
         pointer_accel 0
       }
+
       input "type:keyboard" {
         xkb_options caps:escape
       }
@@ -78,117 +84,105 @@ in
       }
 
       ### Key bindings
-      #
-      # Basics:
-      #
-          bindsym $mod+Return exec $term
-          bindsym $mod+q kill
-          bindsym $mod+space exec $menu
-          floating_modifier $mod normal
-          bindsym $mod+Shift+c reload
-          bindsym $mod+x exec powermenu
-      #
-      # Moving around:
-      #
-          bindsym $mod+$left focus left
-          bindsym $mod+$down focus down
-          bindsym $mod+$up focus up
-          bindsym $mod+$right focus right
+      floating_modifier $mod normal
+      bindsym {
+        # Basics:
+        $mod+Return exec $term
+        $mod+q kill
+        $mod+space exec $menu
+        $mod+Shift+c reload
+        $mod+x exec powermenu
 
-          bindsym $mod+Left focus left
-          bindsym $mod+Down focus down
-          bindsym $mod+Up focus up
-          bindsym $mod+Right focus right
+        # Moving around:
+        $mod+$left focus left
+        $mod+$down focus down
+        $mod+$up focus up
+        $mod+$right focus right
 
-          bindsym $mod+Shift+$left move left
-          bindsym $mod+Shift+$down move down
-          bindsym $mod+Shift+$up move up
-          bindsym $mod+Shift+$right move right
+        $mod+Left focus left
+        $mod+Down focus down
+        $mod+Up focus up
+        $mod+Right focus right
 
-          bindsym $mod+Shift+Left move left
-          bindsym $mod+Shift+Down move down
-          bindsym $mod+Shift+Up move up
-          bindsym $mod+Shift+Right move right
-      #
-      # Workspaces:
-      #
-          # Switch to workspace
-          bindsym $mod+1 workspace number 1
-          bindsym $mod+2 workspace number 2
-          bindsym $mod+3 workspace number 3
-          bindsym $mod+4 workspace number 4
-          bindsym $mod+5 workspace number 5
-          bindsym $mod+6 workspace number 6
-          bindsym $mod+7 workspace number 7
-          bindsym $mod+8 workspace number 8
-          bindsym $mod+9 workspace number 9
-          bindsym $mod+0 workspace number 10
+        $mod+Shift+$left move left
+        $mod+Shift+$down move down
+        $mod+Shift+$up move up
+        $mod+Shift+$right move right
 
-          # Move focused container to workspace
-          bindsym $mod+Shift+1 move container to workspace number 1
-          bindsym $mod+Shift+2 move container to workspace number 2
-          bindsym $mod+Shift+3 move container to workspace number 3
-          bindsym $mod+Shift+4 move container to workspace number 4
-          bindsym $mod+Shift+5 move container to workspace number 5
-          bindsym $mod+Shift+6 move container to workspace number 6
-          bindsym $mod+Shift+7 move container to workspace number 7
-          bindsym $mod+Shift+8 move container to workspace number 8
-          bindsym $mod+Shift+9 move container to workspace number 9
-          bindsym $mod+Shift+0 move container to workspace number 10
-      #
-      # Layout stuff:
-      #
-          bindsym $mod+b splith
-          bindsym $mod+v splitv
+        $mod+Shift+Left move left
+        $mod+Shift+Down move down
+        $mod+Shift+Up move up
+        $mod+Shift+Right move right
 
-          bindsym $mod+s layout stacking
-          bindsym $mod+w layout tabbed
-          bindsym $mod+e layout toggle split
+        # Workspaces:
+        # Switch to workspace
+        $mod+1 workspace number 1
+        $mod+2 workspace number 2
+        $mod+3 workspace number 3
+        $mod+4 workspace number 4
+        $mod+5 workspace number 5
+        $mod+6 workspace number 6
+        $mod+7 workspace number 7
+        $mod+8 workspace number 8
+        $mod+9 workspace number 9
+        $mod+0 workspace number 10
 
-          bindsym $mod+f fullscreen
-          bindsym $mod+Shift+space floating toggle
-          bindsym $mod+t focus mode_toggle
-          bindsym $mod+a focus parent
-      #
-      # Scratchpad:
-      #
-          bindsym $mod+Shift+minus move scratchpad
-          bindsym $mod+minus scratchpad show
-      #
+        # Move focused container to workspace
+        $mod+Shift+1 move container to workspace number 1
+        $mod+Shift+2 move container to workspace number 2
+        $mod+Shift+3 move container to workspace number 3
+        $mod+Shift+4 move container to workspace number 4
+        $mod+Shift+5 move container to workspace number 5
+        $mod+Shift+6 move container to workspace number 6
+        $mod+Shift+7 move container to workspace number 7
+        $mod+Shift+8 move container to workspace number 8
+        $mod+Shift+9 move container to workspace number 9
+        $mod+Shift+0 move container to workspace number 10
+
+        # Layout stuff:
+        $mod+b splith
+        $mod+v splitv
+
+        $mod+s layout stacking
+        $mod+w layout tabbed
+        $mod+e layout toggle split
+
+        $mod+f fullscreen
+        $mod+Shift+space floating toggle
+        $mod+t focus mode_toggle
+        $mod+a focus parent
+
+        # Scratchpad:
+        $mod+Shift+minus move scratchpad
+        $mod+minus scratchpad show
+
+        # Screenshot and Volume
+        --no-repeat Print exec grimshot --notify savecopy
+        --no-repeat Shift+Print exec grimshot --notify savecopy area
+        --no-repeat Ctrl+Print exec grimshot --notify savecopy window
+        XF86AudioRaiseVolume exec changevolume up 5
+        XF86AudioLowerVolume exec changevolume down 5
+        XF86AudioMute exec changevolume mute
+      }
+
       # Resizing containers:
-      #
       mode "resize" {
-          bindsym $left resize shrink width 10px
-          bindsym $down resize grow height 10px
-          bindsym $up resize shrink height 10px
-          bindsym $right resize grow width 10px
+        bindsym {
+          $left resize shrink width 10px
+          $down resize grow height 10px
+          $up resize shrink height 10px
+          $right resize grow width 10px
 
-          bindsym Left resize shrink width 10px
-          bindsym Down resize grow height 10px
-          bindsym Up resize shrink height 10px
-          bindsym Right resize grow width 10px
+          Left resize shrink width 10px
+          Down resize grow height 10px
+          Up resize shrink height 10px
+          Right resize grow width 10px
 
-          bindsym Return mode "default"
-          bindsym Escape mode "default"
+          Return mode "default"
+          Escape mode "default"
+        }
       }
       bindsym $mod+r mode "resize"
-
-      #
-      # Screenshot and Volume
-      #
-          bindsym Print exec grimshot --notify savecopy
-          bindsym Shift+Print exec grimshot --notify savecopy area
-          bindsym Ctrl+Print exec grimshot --notify savecopy window
-          bindsym XF86AudioRaiseVolume exec changevolume up 5
-          bindsym XF86AudioLowerVolume exec changevolume down 5
-          bindsym XF86AudioMute exec changevolume mute
-
-      #
-      # Status Bar:
-      #
-      bar {
-        swaybar_command waybar
-      }
     '';
   };
 }
