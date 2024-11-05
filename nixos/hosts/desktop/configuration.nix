843ddba70c0cbf8abd41a1ea8a8c8031b20ea85b
khsaad04 <khsaad0404@gmail.com>
2024-11-05 17:02:25 +0600

    feat: Move all home-manager packages to nixos configuration

--- nixos/hosts/desktop/configuration.nix
+++ nixos/hosts/desktop/configuration.nix
@@ -1,4 +1,9 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 {
   programs = {
     sway = {
@@ -64,6 +69,60 @@
         "networkmanager"
         "wheel"
         "adbusers"
+      ];
+      packages = [
+        pkgs.wl-clipboard
+        pkgs.qbittorrent
+        pkgs.pavucontrol
+        pkgs.cargo
+        pkgs.gcc
+        pkgs.python3
+        inputs.self.packages.${pkgs.system}.formatter
+        inputs.self.packages.${pkgs.system}.changevolume
+        inputs.self.packages.${pkgs.system}.powermenu
+
+        pkgs.sway
+        pkgs.wl-clip-persist
+        pkgs.findutils
+        pkgs.sway-contrib.grimshot
+        pkgs.adw-gtk3
+        pkgs.adwaita-icon-theme
+        pkgs.xorg.xrdb
+        pkgs.qt6Packages.qtstyleplugin-kvantum
+        pkgs.qt6Packages.qt6ct
+        pkgs.libsForQt5.qtstyleplugin-kvantum
+        pkgs.libsForQt5.qt5ct
+        pkgs.firefox-bin
+        pkgs.fish
+        pkgs.foot
+        pkgs.neovim
+        pkgs.luajitPackages.jsregexp
+        pkgs.gcc
+        pkgs.gnumake
+        pkgs.unzip
+        pkgs.tree-sitter
+        pkgs.ripgrep
+        pkgs.fd
+        pkgs.lua-language-server
+        pkgs.nil
+        pkgs.stylua
+        pkgs.prettierd
+        pkgs.starship
+        pkgs.tmux
+        pkgs.tmux-sessionizer
+        pkgs.tofi
+        pkgs.waybar
+        pkgs.playerctl
+        pkgs.yazi
+        pkgs.zathura
+        pkgs.obs-studio
+        pkgs.mpv
+        pkgs.feh
+        pkgs.git
+        pkgs.dunst
+        pkgs.libnotify
+        pkgs.udiskie
+        pkgs.networkmanagerapplet
       ];
     };
   };
