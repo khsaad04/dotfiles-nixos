6a6ea66d624c22b50dea32f1909e97a8e81a4a36
khsaad04 <khsaad0404@gmail.com>
2024-11-10 19:35:41 +0600

    refactor(desktop): Organize configuration.nix

--- nixos/hosts/desktop/configuration.nix
+++ nixos/hosts/desktop/configuration.nix
@@ -24,8 +24,7 @@
   };
 
   services.fstrim.enable = true;
-  security.polkit.enable = true;
-  hardware.graphics.enable = true;
+  services.udisks2.enable = true;
   networking.networkmanager.enable = true;
   time.timeZone = "Asia/Dhaka";
 
@@ -34,7 +33,85 @@
       EDITOR = "nvim";
       VISUAL = "nvim";
     };
+    systemPackages = [
+      pkgs.xdg-utils
+      pkgs.wget
+      pkgs.file
+      pkgs.fd
+      pkgs.ripgrep
+      pkgs.btop
+      pkgs.qemu
+      (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+        qemu-system-x86_64 \
+          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+          "$@"
+      '')
+      pkgs.wl-clipboard
+      pkgs.qbittorrent
+      pkgs.pavucontrol
+      pkgs.cargo
+      pkgs.gcc
+      pkgs.python3
+      inputs.self.packages.${pkgs.system}.formatter
+      inputs.self.packages.${pkgs.system}.changevolume
+      inputs.self.packages.${pkgs.system}.powermenu
+      inputs.dotman.packages.${pkgs.system}.default
+
+      # sway
+      pkgs.wl-clip-persist
+      pkgs.findutils
+      pkgs.sway-contrib.grimshot
+
+      # theming
+      pkgs.adw-gtk3
+      pkgs.adwaita-icon-theme
+      pkgs.xorg.xrdb
+      pkgs.qt6Packages.qtstyleplugin-kvantum
+      pkgs.qt6Packages.qt6ct
+      pkgs.libsForQt5.qtstyleplugin-kvantum
+      pkgs.libsForQt5.qt5ct
+
+      # neovim
+      pkgs.neovim
+      pkgs.luajitPackages.jsregexp
+      pkgs.gcc
+      pkgs.gnumake
+      pkgs.unzip
+      pkgs.tree-sitter
+      pkgs.ripgrep
+      pkgs.fd
+      pkgs.lua-language-server
+      pkgs.nil
+      pkgs.stylua
+      pkgs.prettierd
+
+      pkgs.firefox-bin
+      pkgs.foot
+      pkgs.starship
+      pkgs.tmux
+      pkgs.tmux-sessionizer
+      pkgs.tofi
+      pkgs.waybar
+      pkgs.playerctl
+      pkgs.yazi
+      pkgs.zathura
+      pkgs.obs-studio
+      pkgs.mpv
+      pkgs.feh
+      pkgs.dunst
+      pkgs.libnotify
+      pkgs.udiskie
+      pkgs.networkmanagerapplet
+    ];
   };
+
+  programs.dconf.profiles.user.databases = [
+    {
+      settings."org/gnome/desktop/interface" = {
+        gtk-theme = "adw-gtk3-dark";
+      };
+    }
+  ];
 
   users = {
     mutableUsers = false;
@@ -48,84 +125,8 @@
         "wheel"
         "adbusers"
       ];
-      packages = [
-        pkgs.xdg-utils
-        pkgs.vim
-        pkgs.wget
-        pkgs.file
-        pkgs.fd
-        pkgs.ripgrep
-        pkgs.btop
-        pkgs.qemu
-        (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
-          qemu-system-x86_64 \
-            -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
-            "$@"
-        '')
-        pkgs.wl-clipboard
-        pkgs.qbittorrent
-        pkgs.pavucontrol
-        pkgs.cargo
-        pkgs.gcc
-        pkgs.python3
-        inputs.self.packages.${pkgs.system}.formatter
-        inputs.self.packages.${pkgs.system}.changevolume
-        inputs.self.packages.${pkgs.system}.powermenu
-        inputs.dotman.packages.${pkgs.system}.default
-
-        pkgs.sway
-        pkgs.wl-clip-persist
-        pkgs.findutils
-        pkgs.sway-contrib.grimshot
-        pkgs.adw-gtk3
-        pkgs.adwaita-icon-theme
-        pkgs.xorg.xrdb
-        pkgs.qt6Packages.qtstyleplugin-kvantum
-        pkgs.qt6Packages.qt6ct
-        pkgs.libsForQt5.qtstyleplugin-kvantum
-        pkgs.libsForQt5.qt5ct
-        pkgs.firefox-bin
-        pkgs.fish
-        pkgs.foot
-        pkgs.neovim
-        pkgs.luajitPackages.jsregexp
-        pkgs.gcc
-        pkgs.gnumake
-        pkgs.unzip
-        pkgs.tree-sitter
-        pkgs.ripgrep
-        pkgs.fd
-        pkgs.lua-language-server
-        pkgs.nil
-        pkgs.stylua
-        pkgs.prettierd
-        pkgs.starship
-        pkgs.tmux
-        pkgs.tmux-sessionizer
-        pkgs.tofi
-        pkgs.waybar
-        pkgs.playerctl
-        pkgs.yazi
-        pkgs.zathura
-        pkgs.obs-studio
-        pkgs.mpv
-        pkgs.feh
-        pkgs.git
-        pkgs.dunst
-        pkgs.libnotify
-        pkgs.udiskie
-        pkgs.networkmanagerapplet
-      ];
     };
   };
-
-  programs.dconf.profiles.user.databases = [
-    {
-      settings."org/gnome/desktop/interface" = {
-        gtk-theme = "adw-gtk3-dark";
-      };
-    }
-  ];
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
